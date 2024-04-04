9b5da627192ea3bd60230267767de529fb5f7ba9
anoBand <125958306+anoBand@users.noreply.github.com>
2024-04-04 17:06:25 +0900

    Title: 합과 평균의 차, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240404/합과 평균의 차/sub-of-average-and-sum.c
+++ 240404/합과 평균의 차/sub-of-average-and-sum.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a,b,c,d;
+    int a,b,c;
     scanf("%d %d %d",a,b,c);
-    d=a+b+c;
-    printf("%d\n%d\n%d",d,d/3,d-d/3);
+    printf("%d \n%d \n%d ",a+b+c,(a+b+c)/3,a+b+c-((a+b+c)/3));
     return 0;
 }
