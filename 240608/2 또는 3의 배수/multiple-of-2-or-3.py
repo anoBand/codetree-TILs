e355de4b1e55bd3b58ee51ca718496963711c1b4
anoBand <125958306+anoBand@users.noreply.github.com>
2024-06-08 22:51:05 +0900

    Title: 2 또는 3의 배수, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240608/2 또는 3의 배수/multiple-of-2-or-3.py
+++ 240608/2 또는 3의 배수/multiple-of-2-or-3.py
@@ -2,6 +2,6 @@ n=int(input())
 
 for i in range (1,n):
     if(i%2==0 or i%3==0):
-        print('1',end='')
+        print('1 ',end='')
     else:
-        print('0',end='')
+        print('0 ',end='')
