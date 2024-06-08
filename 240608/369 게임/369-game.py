a96670091efc7965d5f68fb7d025a2bdefa8409b
anoBand <125958306+anoBand@users.noreply.github.com>
2024-06-08 23:17:03 +0900

    Title: 369 게임, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240608/369 게임/369-game.py
+++ 240608/369 게임/369-game.py
@@ -2,7 +2,7 @@ n=int(input())
 
 for i in range(1,n+1):
     str_i=str(i)
-    if( '3' in str_i or '6' in str_i or '9' in str_i):
+    if( i%3==0 or '3' in str_i or '6' in str_i or '9' in str_i):
         print(0,'',end='')
     else:
         print(i,'',end='')
