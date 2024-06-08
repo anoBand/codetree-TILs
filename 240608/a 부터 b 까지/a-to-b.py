3ade2c7d312a8f510781b58021123818983b9779
anoBand <125958306+anoBand@users.noreply.github.com>
2024-06-08 22:59:27 +0900

    Title: a 부터 b 까지, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240608/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,14 @@
+inp=input()
+arr=inp.split()
+a=int(arr[0])
+b=int(arr[1])
+num=a
+while True:
+    if(num>b):
+        break
+    else:
+        print(num)
+        if(num%2==1):
+            num*=2
+        else:
+            num+=3
