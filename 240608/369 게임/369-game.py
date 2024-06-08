4ba32924787000532b6b558973a1770f893b1f2c
anoBand <125958306+anoBand@users.noreply.github.com>
2024-06-08 23:16:00 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240608/369 게임/369-game.py
+++ 240608/369 게임/369-game.py
@@ -2,7 +2,7 @@ n=int(input())
 
 for i in range(1,n+1):
     str_i=str(i)
-    if(i%3==0 or str_i=='3'or str_i=='6'or str_i=='9'):
+    if( '3' in str_i or '6' in str_i or '9' in str_i):
         print(0,'',end='')
     else:
         print(i,'',end='')
